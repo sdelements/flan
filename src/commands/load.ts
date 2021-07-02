704b585250df11b16c0122d2a1885679ea480467
Hristina Iordanova <hiordanova@securitycompass.com>
2021-07-02 15:04:26 -0400

    Resolve package conflict.

---
+++ src/commands/load.ts
@@ -0,0 +1,29 @@
+import {Command} from '@oclif/command'
+import * as execa from 'execa'
+
+export default class Load extends Command {
+  static description = 'describe the command here'
+
+  static examples = [
+    `$ tart save`,
+  ]
+
+  static args = [{
+      name: 'input', 
+      required: false,
+      description: 'name of input file',
+      default: 'dbdump'
+    }]
+
+  async run() {
+    const {args} = this.parse(Load)
+
+    const input = args.input
+    this.log(`output file name: ${input}`)
+
+    const dump = await execa.command(
+      `pg_restore --jobs=8 -U sdlc -d sdlc --clean ${input}`
+    );
+
+  }
+}
---
+++ src/commands/save.ts
@@ -0,0 +1,29 @@
+import {Command} from '@oclif/command'
+import * as execa from 'execa'
+
+export default class Save extends Command {
+  static description = 'describe the command here'
+
+  static examples = [
+    `$ tart save`,
+  ]
+
+  static args = [{
+      name: 'output', 
+      required: false,
+      description: 'name of output file',
+      default: 'dbdump'
+    }]
+
+  async run() {
+    const {args} = this.parse(Save)
+
+    const output = args.output
+    this.log(`output file name: ${output}`)
+
+    const dump = await execa.command(
+      `pg_dump -Fc -Z 9 -U sdlc --file=${output} sdlc`
+    );
+
+  }
+}
