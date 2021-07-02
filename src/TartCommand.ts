642fd7a2d2a254bdfb05fe92d994f232ef7fd0d7
Milena <mdamnjanovic@securitycompass.com>
2021-07-02 14:53:27 -0400

    Add support for config

---
+++ src/TartCommand.ts
@@ -0,0 +1,37 @@
+import { Command, flags } from "@oclif/command";
+import { Input, OutputFlags } from "@oclif/parser";
+import * as fs from "fs-extra";
+
+export default abstract class TartCommand extends Command {
+  localConfig?: {
+    database: { host: string; db: string; user: string; password: string };
+    repository: string;
+    saveDir: string;
+  };
+
+  static flags = {
+    config: flags.string({
+      char: "c",
+      description: "Path to configuration file",
+      env: "TART_CONFIG",
+    }),
+  };
+
+  // https://github.com/oclif/oclif/issues/225
+  protected parsedFlags?: OutputFlags<typeof TartCommand.flags>;
+
+  async init() {
+    const { flags } = this.parse(
+      this.constructor as Input<typeof TartCommand.flags>
+    );
+
+    const configPath = flags.config || "./tart.config.js";
+
+    try {
+      const config = await fs.readJson(configPath as string);
+      this.localConfig = config;
+    } catch (err) {
+      this.error("Unable to load configuration", err);
+    }
+  }
+}
--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,31 +1,34 @@
-import {Command, flags} from '@oclif/command'
+import { flags } from "@oclif/command";
+import TartCommand from "../TartCommand";
 
-export default class Hello extends Command {
-  static description = 'describe the command here'
+export default class Hello extends TartCommand {
+  static description = "describe the command here";
 
   static examples = [
     `$ tart hello
 hello world from ./src/hello.ts!
 `,
-  ]
+  ];
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    ...TartCommand.flags,
+    help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    force: flags.boolean({ char: "f" }),
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
   async run() {
-    const {args, flags} = this.parse(Hello)
+    const { args, flags } = this.parse(Hello);
+    console.log(this.localConfig);
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
+    const name = flags.name ?? "world";
+    this.log(`hello ${name} from ./src/commands/hello.ts`);
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
