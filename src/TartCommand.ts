4d845dbb128e4c5f739a7141a99b07a3f845d63e
Milena <mdamnjanovic@securitycompass.com>
2021-07-02 16:04:15 -0400

    Fix ts errors and update config loading

--- src/TartCommand.ts
+++ src/TartCommand.ts
@@ -1,12 +1,13 @@
 import { Command, flags } from "@oclif/command";
+import { IConfig } from "@oclif/config";
 import { Input, OutputFlags } from "@oclif/parser";
 import * as fs from "fs-extra";
 
 export default abstract class TartCommand extends Command {
-  localConfig?: {
-    database: { host: string; db: string; user: string; password: string };
-    repository: string;
+  localConfig: {
+    database: { host: string; db?: string; user?: string; password?: string };
     saveDir: string;
+    repository?: string;
   };
 
   static flags = {
@@ -14,22 +15,36 @@ export default abstract class TartCommand extends Command {
       char: "c",
       description: "Path to configuration file",
       env: "TART_CONFIG",
+      default: "./tart.config.json",
     }),
   };
 
   // https://github.com/oclif/oclif/issues/225
   protected parsedFlags?: OutputFlags<typeof TartCommand.flags>;
 
+  constructor(argv: string[], config: IConfig) {
+    super(argv, config);
+
+    this.localConfig = {
+      saveDir: ".tart",
+      database: {
+        host: "localhost",
+      },
+    };
+  }
+
   async init() {
     const { flags } = this.parse(
       this.constructor as Input<typeof TartCommand.flags>
     );
 
-    const configPath = flags.config || "./tart.config.js";
+    const configJSON = await fs.readJson(flags.config as unknown as string);
 
     try {
-      const config = await fs.readJson(configPath as string);
-      this.localConfig = config;
+      this.localConfig = {
+        ...this.localConfig,
+        ...configJSON,
+      };
     } catch (err) {
       this.error("Unable to load configuration", err);
     }
