de9a291a0d0875631c3f4b4d9dbb45043b5905ec
Nar Saynorath <nsaynorath@securitycompass.com>
2021-07-08 11:28:12 -0400

    Add delete for local-only db dumps

---
+++ src/commands/delete.ts
@@ -0,0 +1,50 @@
+import * as path from "path";
+import * as fs from "fs-extra";
+import cli from "cli-ux";
+
+import TartCommand from "../TartCommand";
+
+export default class Load extends TartCommand {
+  static description = "delete a local database dump";
+
+  static examples = ["$ tart delete myDB"];
+
+  static flags = {
+    ...TartCommand.flags,
+  };
+
+  static args = [
+    {
+      name: "deleteFile", // TODO: Maybe a better name?
+      required: true,
+      description: "name of file to delete",
+    },
+  ];
+
+  async run() {
+    await this.runHook("beforeDelete");
+
+    const { args } = this.parse(Load);
+    const { deleteFile } = args;
+
+    const deleteFilePath = path.resolve(this.localConfig.saveDir, deleteFile);
+    if (!(await fs.pathExists(deleteFilePath))) {
+      this.error(`This file does not exist`);
+    }
+
+    if (!(await fs.lstat(deleteFilePath)).isFile()) {
+      this.error(`This is not a valid file`);
+    }
+
+    if (
+      await cli.confirm(
+        `Are you sure you wish to delete the local database dump: ${deleteFile}? [y/n]`
+      )
+    ) {
+      await fs.remove(deleteFilePath);
+      this.log(`Successfully deleted ${deleteFile}`);
+    }
+
+    await this.runHook("afterDelete");
+  }
+}
